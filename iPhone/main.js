fff9e89141972ab52a5fa78184868631a0e1bd62
KARIM_AKL <160742764+Karim-Akl@users.noreply.github.com>
2024-06-13 22:54:42 +0300

    Add files via upload

---
+++ iPhone/main.js
@@ -0,0 +1,11 @@
+let icon=document.getElementById('icon');
+let imgemain=document.getElementById('imge');
+let cont=document.querySelector('.container')
+function getelment(prt){
+    imgemain.src=prt;
+
+
+}
+function coler(coler){
+    cont.style.background=coler;
+}
